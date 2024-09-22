0aa7ec414907863ea890ef1b28dddc80f431c8ef
AndresMosquera17 <aamosquera1@gmail.com>
2024-09-22 00:57:02 -0400

    Updated the Inventory Menu
    I added the inventory menu and put it to have 16 slots, (this can change depending on what we want). However, I wasn't able to run the project to test.

--- scripts/ui/inventory_menu.gd
+++ scripts/ui/inventory_menu.gd
@@ -1,10 +1,25 @@
 class_name InventoryMenu extends Control
 
-@export var is_on : bool = true
+@export var is_on: bool = true
+@onready var inventory_panel = $InventoryPanel
+@onready var grid_container = $InventoryPanel/GridContainer
+
+var max_items = 16
+var item_slots = []
 
 func _ready():
 	is_on = false
+	inventory_panel.visible = false
+	create_inventory_slots()
 
-func _input(event : InputEvent):
+func _input(event: InputEvent):
 	if event.is_action_pressed("open_inventory"):
 		is_on = !is_on
+		inventory_panel.visible = is_on
+
+func create_inventory_slots():
+	for i in range(max_items):
+		var slot = Label.new()
+		slot.text = "Empty"
+		grid_container.add_child(slot)
+		item_slots.append(slot)
