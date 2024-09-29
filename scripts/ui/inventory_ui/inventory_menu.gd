56bb64bc5ce6f0645564369168cf66755048a638
AndresMosquera17 <aamosquera1@gmail.com>
2024-09-29 03:32:47 -0400

    Updated the Inventory UI
    Added a player inventory with 8 available slots. The player can press the 'Y' key to open and close the inventory.

--- scripts/ui/inventory_ui/inventory_menu.gd
+++ scripts/ui/inventory_ui/inventory_menu.gd
@@ -1,25 +1,27 @@
 class_name InventoryMenu extends Control
 
 @export var is_on: bool = true
-@onready var inventory_panel = $InventoryPanel
-@onready var grid_container = $InventoryPanel/GridContainer
+@onready var InventoryPanel = $InventoryPanel
+@onready var GridControl = $InventoryPanel/GridControl
 
-var max_items = 16
+var max_items = 8
 var item_slots = []
 
 func _ready():
 	is_on = false
-	inventory_panel.visible = false
+	InventoryPanel.visible = false
 	create_inventory_slots()
 
 func _input(event: InputEvent):
 	if event.is_action_pressed("open_inventory"):
+		print("Y key pressed")  # Debug message
 		is_on = !is_on
-		inventory_panel.visible = is_on
+		InventoryPanel.visible = is_on
+		print("Inventory is now:", is_on)  # Check the state of the inventory
 
 func create_inventory_slots():
 	for i in range(max_items):
 		var slot = Label.new()
 		slot.text = "Empty"
-		grid_container.add_child(slot)
+		GridControl.add_child(slot)
 		item_slots.append(slot)
